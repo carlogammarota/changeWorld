58afea0894cb5b0423078f669dfae7ac5347c594
CarloGammarota <carlo.gammarota@gmail.com>
2019-03-15 01:50:39 -0300

    first commit

---
+++ src/components/agregarAlumno.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "agregarAlumno"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/imgTest.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "imgTest"
+    }
+</script>
+
+<style scoped>
+
+</style>
